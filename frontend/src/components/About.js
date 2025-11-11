2d7c3bf0ee2097ceff3e68eb4265d9e348f162c3
Danielle Siegel <daniellesiegel78@gmail.com>
2025-11-10 20:27:23 -0500

    trying to add simple navigation but failing miserably

---
+++ frontend/src/components/About.js
@@ -0,0 +1,15 @@
+function About() {
+    return (
+        <div>
+        <about-header>
+            <h2>About</h2>
+            <p>This is the about page.</p>
+            </about-header>
+            <about-body>
+            </about-body>                     
+        </div>
+        
+    );
+}
+
+export default About;
