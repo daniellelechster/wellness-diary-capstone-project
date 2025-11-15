1d0b4a243d106a7082564c0959e10d601bf1b354
marbue123 <mnorm2004@gmail.com>
2025-11-15 17:04:37 -0500

    ok

--- backend/src/main/java/com/wcci/wellness/repository/MoodPopulator.java
+++ backend/src/main/java/com/wcci/wellness/repository/MoodPopulator.java
@@ -1,28 +1,28 @@
-package com.wcci.wellness.repository;
+// package com.wcci.wellness.repository;
 
-import com.wcci.wellness.entity.Mood;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
+// import com.wcci.wellness.entity.Mood;
+// import org.springframework.boot.CommandLineRunner;
+// import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
+// import java.time.LocalDateTime;
 
-@Component
-public class MoodPopulator implements CommandLineRunner {
+// @Component
+// public class MoodPopulator implements CommandLineRunner {
 
-    private final MoodRepository moodRepo; 
+//     private final MoodRepository moodRepo; 
 
-    public MoodPopulator(MoodRepository moodRepo) {
-        this.moodRepo = moodRepo;
-    }
+//     public MoodPopulator(MoodRepository moodRepo) {
+//         this.moodRepo = moodRepo;
+//     }
 
-    @Override
-    public void run(String... args) throws Exception {
+//     @Override
+//     public void run(String... args) throws Exception {
 
-        moodRepo.save(new Mood(LocalDateTime.now().minusDays(5), 1));
-        moodRepo.save(new Mood(LocalDateTime.now().minusDays(4), 3));
-        moodRepo.save(new Mood(LocalDateTime.now().minusDays(3), 5));
-        moodRepo.save(new Mood(LocalDateTime.now().minusDays(2), 7));
-        moodRepo.save(new Mood(LocalDateTime.now().minusDays(1), 9));
-        moodRepo.save(new Mood(LocalDateTime.now(), 4));
-    }
-}
+//         moodRepo.save(new Mood(LocalDateTime.now().minusDays(5), 1));
+//         moodRepo.save(new Mood(LocalDateTime.now().minusDays(4), 3));
+//         moodRepo.save(new Mood(LocalDateTime.now().minusDays(3), 5));
+//         moodRepo.save(new Mood(LocalDateTime.now().minusDays(2), 7));
+//         moodRepo.save(new Mood(LocalDateTime.now().minusDays(1), 9));
+//         moodRepo.save(new Mood(LocalDateTime.now(), 4));
+//     }
+// }
