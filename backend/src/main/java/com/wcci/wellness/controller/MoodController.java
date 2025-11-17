20b26f1007fed54f3b3a48aa5c2613cc6878eb32
Jaysen King <sortingsin@gmail.com>
2025-11-17 09:45:43 -0500

    Updated mood.java, pulled originmain commits put in since initial PR was put in

--- backend/src/main/java/com/wcci/wellness/controller/MoodController.java
+++ backend/src/main/java/com/wcci/wellness/controller/MoodController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/wellness")
+@RequestMapping("/api/mood")
 public class MoodController {
     
     private final MoodService moodService;
