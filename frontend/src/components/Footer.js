b0cb93440cb88791d89106b1dfcae2d285dc3eb4
Kayla Buckner <kaylambuckner33@gmail.com>
2025-11-17 20:29:42 -0500

    pushing

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,6 @@ import React from 'react'
 function Footer() {
     return (
         <nav class="footer-container">
-        <nav class="footer-container">
         <footer>
             <p>&copy; 2025 CodeBusters</p>
             
