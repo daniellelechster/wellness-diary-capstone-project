b317100a558bdd584641632a282b665432ea2ae7
Jaysen King <sortingsin@gmail.com>
2025-11-19 15:08:40 -0500

    Goal Entity created

---
+++ backend/src/main/java/com/wcci/wellness/entity/Goal.java
@@ -0,0 +1,57 @@
+package com.wcci.wellness.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Goal {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String text;
+    private String status;
+
+    public Goal() {
+        
+    }
+
+    public Goal(String text, String status) {
+        this.text = text;
+        this.status = status;
+    }
+    
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDate() {
+        return text;
+    }
+
+    public void setDate(String text) {
+        this.text = text;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }    
+}
