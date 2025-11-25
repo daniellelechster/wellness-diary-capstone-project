cfdd02b3d9908d03c50931a11ae93d6ad4666442
Jaysen King <sortingsin@gmail.com>
2025-11-25 11:22:05 -0500

    Exercise Packages completed. Edited for more robust PostMan testing.

---
+++ backend/src/main/java/com/wcci/wellness/service/ExerciseService.java
@@ -0,0 +1,20 @@
+package com.wcci.wellness.service;
+
+import com.wcci.wellness.entity.Exercise;
+import java.time.LocalDate;
+import java.util.List;
+
+public interface ExerciseService {
+
+    Exercise getExerciseById(Long id);
+
+    Exercise getExerciseByDate(LocalDate date);
+
+    List<Exercise> getAllExercises();
+
+    Exercise saveExercise(Exercise Exercise);
+
+    Exercise logMinutes(LocalDate date, int minutes);
+
+    Exercise toggleCompleted(LocalDate date);
+}
